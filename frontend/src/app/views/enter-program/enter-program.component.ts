28b058210e7b2d63418773346f716c5df4ef29e0
ayushman1024 <ayushman1024@gmail.com>
2019-05-15 20:49:05 +0530

    # Validation and error message for "enter-program.component"

--- frontend/src/app/views/enter-program/enter-program.component.ts
+++ frontend/src/app/views/enter-program/enter-program.component.ts
@@ -1,19 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { ProgramService } from 'src/app/framework/services/program.service';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-enter-program',
   templateUrl: './enter-program.component.html',
-  styleUrls: ['./enter-program.component.css']
+  styleUrls: ['./enter-program.component.css'],
+  providers: [MessageService]
 })
 export class EnterProgramComponent implements OnInit {
 
   passcode: string;
-  constructor(private prgService: ProgramService) { }
+  constructor(private msg: MessageService, private prgService: ProgramService) { }
 
   ngOnInit() {
   }
   send() {
-    this.prgService.enterPass(this.passcode).subscribe(data => {}, error => {});
+    this.prgService.enterPass(this.passcode).subscribe(data => {
+      this.msg.add({severity: 'success', summary: 'Service Message', detail: 'You are now member of this program'});
+    }, error => {
+      this.msg.add({severity: 'error', summary: 'Service Message', detail: 'Wrong Passcode'});
+    });
   }
 }

a900097c889e42dbd2321a0ed586f1c8b6e9c16e
ayushman1024 <ayushman1024@gmail.com>
2019-05-15 20:47:48 +0530

    # frontend VALIDATION for "create-program" form

--- frontend/src/app/views/create-program/create-program.component.ts
+++ frontend/src/app/views/create-program/create-program.component.ts
@@ -5,11 +5,13 @@ import { ProgramService } from '../../framework/services/program.service';
 import { NewProgram } from '../../framework/models/NewProgram';
 import { User } from 'src/app/framework/models/user';
 import { UserService } from 'src/app/framework/services/user.service';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-create-program',
   templateUrl: './create-program.component.html',
-  styleUrls: ['./create-program.component.css']
+  styleUrls: ['./create-program.component.css'],
+  providers: [MessageService]
 })
 export class CreateProgramComponent implements OnInit {
   sourceUser: User[];
@@ -17,7 +19,7 @@ export class CreateProgramComponent implements OnInit {
   newProgram = new NewProgram();
   isLinear = true;
 
-  constructor(private service: ProgramService, private userService: UserService,
+  constructor(private msg: MessageService, private service: ProgramService, private userService: UserService,
               private datePipe: DatePipe, private global: GlobalService) {}
 
   ngOnInit() {
@@ -31,10 +33,20 @@ export class CreateProgramComponent implements OnInit {
   }
 
   onSubmit() {
-    this.newProgram.users = [];
-    this.assignedUser.forEach(u => {this.newProgram.users.push(u.id); });
-    this.newProgram.admin = this.global.getUid();
-    this.service.create(this.newProgram).subscribe();
+    if ((this.newProgram.name.length > 2) && this.newProgram.description.length > 0) {
+      this.newProgram.users = [];
+      this.assignedUser.forEach(u => {this.newProgram.users.push(u.id); });
+      this.newProgram.admin = this.global.getUid();
+      this.service.create(this.newProgram).subscribe();
+    } else {
+      if (!(this.newProgram.name.length > 2)) {
+        this.msg.add({severity: 'error', summary: 'Invalid Name', detail: 'Name field is blank'});
+      }
+      if (!this.newProgram.description) {
+        this.msg.add({severity: 'error', summary: 'Invalid Description', detail: 'No description is provided'});
+      }
+    }
+
   }
 
 }

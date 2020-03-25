4ab67fa422fa03415e0cb0e70481aca321c4f51a
Ayushman Srivastava <ayushman1024@gmail.com>
2020-03-25 21:00:43 +0530

    FIX for issue#7 :  Session is destroyed after page is refreshed

--- frontend/src/app/framework/services/global.service.ts
+++ frontend/src/app/framework/services/global.service.ts
@@ -16,39 +16,57 @@ export class GlobalService {
   private logged = false;
   private selectedTask: Task;
   getToken(): string {
+    this.token = sessionStorage.getItem('token');
     return this.token;
   }
   setToken(tokenValue: string) {
     this.token = tokenValue;
+    sessionStorage.setItem('token', this.token);
   }
   getCurrentProgramId(): number {
+    this.currentProgramId = +sessionStorage.getItem('currentProgramID');
     return this.currentProgramId;
   }
   setCurrentProgramId(programId: number) {
     this.currentProgramId = programId;
+    sessionStorage.setItem('currentProgramID', '' + this.currentProgramId);
   }
   getUid(): number {
+    this.uid = +sessionStorage.getItem('uid');
     return this.uid;
   }
   setUid(uid: number) {
     this.uid = uid;
+    sessionStorage.setItem('uid', '' + this.uid);
   }
   getSUid(): number {
+    this.suid = +sessionStorage.getItem('suid');
     return this.suid;
   }
   setSUid(suid: number) {
     this.suid = suid;
+    sessionStorage.setItem('suid', '' + this.suid);
   }
   getProgramName(): string {
+    this.programName = sessionStorage.getItem('programName');
     return this.programName;
   }
   setProgramName(programName: string) {
     this.programName = programName;
+    sessionStorage.setItem('programName', this.programName);
   }
   setLogged(v: boolean) {
+    if ( !v ) {
+      sessionStorage.clear();
+      sessionStorage.setItem('logged', 'false');    // again created "logged" sessionstorage item, bcoz it is needed to be checked
+    } else {
+      sessionStorage.setItem('logged', 'true');
+    }
     this.logged = v;
   }
   getLogged(): boolean {
+    if ( sessionStorage.getItem('logged') === 'true') { this.logged = true; }
+    if ( sessionStorage.getItem('logged') === 'false') { this.logged = false; }
     return this.logged;
   }
 

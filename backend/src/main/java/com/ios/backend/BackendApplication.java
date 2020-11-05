217743664c6b4af3c7710bb56a42ce8c4bf4a022
Ayushman Srivastava <ayushman1024@gmail.com>
2020-11-05 21:25:31 +0530

    Major changes, Main class, application.properties, .gitignore

--- backend/src/main/java/com/ios/backend/BackendApplication.java
+++ backend/src/main/java/com/ios/backend/BackendApplication.java
@@ -2,8 +2,10 @@ package com.ios.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application.properties")
 public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
